b96c7a41b2fb7b88c32959e4b0d67d1f6b9ac895
NisseJesper <nisssen2@gmail.com>
2024-02-05 13:10:50 +0100

    Nav top and bottom

--- homeCode.js
+++ homeCode.js
@@ -1,8 +1,30 @@
-function revealSidebar() {
-    var element = document.getElementById("sidebar");
-    if (element.style.display === "none") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
-    }
-}
+const expand_btn = document.querySelector("expand-btn");
+
+let activeIndex;
+
+expand_btn.addEventListener("click", () => {
+    document.body.classList.toggle("collapsed");
+});
+
+const current = window.location.href;
+const allLinks = document.querySelectorAll(".sidebar-link a");
+
+allLinks.forEach((elem) => {
+    elem.addEventListener("click", function () {
+        const hrefLinkClick = elem.href;
+
+        allLinks.forEach((link) => {
+            if (link.href == hrefLinkClick) {
+                link.classList.add("active");
+    
+            } else {
+                link.classList.remove("active")
+            }
+        })
+    })
+})
+
+const SearchInput = document.querySelector(".search-wrap input");
+SearchInput.addEventListener("focus", (e) => {
+    document.body.classList.remove("collapsed");
+});
